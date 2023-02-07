3bfa6059755acf0b0e571c3c3b7222cbf59223f4
Gary Hughes <gclm.automation@outlook.com>
2023-02-07 15:27:39 +0000

    First Commit of impossible button, also contains DOM generated boxes for practice

---
+++ app.js
@@ -0,0 +1,37 @@
+// const btn = document.querySelector('button');
+
+// btn.addEventListener(`mouseover`, function(){
+// 	console.log(`Moused over me!`)
+// 	const height = Math.floor(Math.random() * window.innerHeight);
+// 	const width = Math.floor(Math.random() * window.innerWidth);
+// 	btn.style.left = `${width}px`;
+// 	btn.style.top = `${height}px`;
+// });
+
+// btn.addEventListener(`click`, function() {
+// 	alert("You got the button!");
+// 	document.body.style.backgroundColor = `green`;
+// })
+
+
+
+//////////////////////////////////////////////////////////////////
+
+
+// const colors = [`red`, `green`, `blue`, `yellow`, `orange`, `purple`];
+
+// const changeColor = function(box){
+// 	const h1 = document.querySelector("h1");
+// 	h1.style.color = this.style.backgroundColor
+// }
+
+// const h1 = document.querySelector("h1");
+// const container = document.querySelector(`#boxes`);
+
+// for(let color of colors){
+// 	const box = document.createElement(`div`);
+// 	box.style.backgroundColor = color;
+// 	box.classList.add(`box`);
+// 	container.appendChild(box);
+// 	box.addEventListener(`click`, changeColor)
+// }
